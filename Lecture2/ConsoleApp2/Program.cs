87fd9724a7f552c5ba2fcbfac0eb5583143590fb
Hiyakku <90826568+Hiyakku@users.noreply.github.com>
2021-09-30 09:46:39 +0200

    Add files via upload

---
+++ Lecture2/ConsoleApp2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("operator pls:");
+                string plusnebominus = Console.ReadLine();
+                if (plusnebominus != "+" && (plusnebominus != "-") && (plusnebominus != "*") && (plusnebominus != "/"))
+                {
+                    Console.WriteLine("sorry, wrong operator. I shall now terminate");
+                    Console.ReadLine();
+                    System.Environment.Exit(0);
+                }
+                Console.WriteLine("nümber one pls:");
+                double num1 = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+
+                Console.WriteLine("nümber two:");
+                double num2 = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+
+                double result;
+
+                if (plusnebominus == "+")
+                {
+                    result = num1 + num2;
+                }
+                else if (plusnebominus == "-")
+                {
+                    result = num1 - num2;
+                }
+                else if (plusnebominus == "*")
+                {
+                    result = num1 * num2;
+                }
+                else if (plusnebominus == "/")
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    result = 0;
+                }
+
+                Console.WriteLine(result);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
