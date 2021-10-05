eac509e6ba3217874f20aaa1b02459581e104135
Hiyakku <90826568+Hiyakku@users.noreply.github.com>
2021-10-05 15:04:41 +0200

    Add files via upload

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace nocluewhatimdoing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a number please:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (x % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (x % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else Console.WriteLine("neither Fizz, nor Buzz");
+
+
+            Console.ReadLine();
+        }
+    }
+}
